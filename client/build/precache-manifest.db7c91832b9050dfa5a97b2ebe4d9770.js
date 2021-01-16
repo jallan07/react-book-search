bc3859a52febdb0cb7aecf26d1d3c87a0f2d85e1
Josh Allan <allan.josh07@gmail.com>
2021-01-16 13:37:31 -0500

    upload of files

---
+++ client/build/precache-manifest.db7c91832b9050dfa5a97b2ebe4d9770.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3da62768a60c7a40c1dcc40b2d90075d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad48689a4e6d48ed5473",
+    "url": "/static/css/main.997d4878.chunk.css"
+  },
+  {
+    "revision": "9a639d82447ade4efadb",
+    "url": "/static/js/2.c162a99d.chunk.js"
+  },
+  {
+    "revision": "223be75f6809993b56a94be00d710bed",
+    "url": "/static/js/2.c162a99d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad48689a4e6d48ed5473",
+    "url": "/static/js/main.00818b31.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
